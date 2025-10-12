4cfd53adebc8507d51efdc1f8ddb4f8ffe5f4acf
JWI <jwi@j-w-i.org>
2025-10-13 07:33:44 +1100

    a

--- Sources/SFSymbolsCompatKit/SFSymbolsCompatKit.swift
+++ Sources/SFSymbolsCompatKit/SFSymbolsCompatKit.swift
@@ -105,48 +105,31 @@ public extension UIImage {
         }
 
         guard let unicode = SFSymbols.shared.unicode(for: name),
-              let font = SFSymbols.shared.font(weight: config.weight, size: fontSize) else {
-            return nil
-        }
+              let font = SFSymbols.shared.font(weight: config.weight, size: fontSize) else { return nil }
 
         let attrString = NSAttributedString(string: unicode, attributes: [
             .font: font,
             .foregroundColor: UIColor.black
         ])
 
-        let line = CTLineCreateWithAttributedString(attrString)
-        let runs = CTLineGetGlyphRuns(line) as! [CTRun]
+        let imageSize = attrString.size()
 
-        var tightBounds = CGRect.null
-        for run in runs {
-            let runCount = CTRunGetGlyphCount(run)
-            for i in 0..<runCount {
-                let glyphBounds = CTRunGetImageBounds(run, nil, CFRange(location: i, length: 1))
-                tightBounds = tightBounds.union(glyphBounds)
-            }
-        }
-
-        // Slight padding to prevent clipping
-        let scale = UIScreen.main.scale
-        let inset: CGFloat = 1.5 / scale
-        tightBounds = tightBounds.insetBy(dx: -inset, dy: -inset)
-
-        UIGraphicsBeginImageContextWithOptions(tightBounds.size, false, scale)
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0)
         guard let context = UIGraphicsGetCurrentContext() else { return nil }
 
-        // Fill the background with red
+        // Fill background with red
         UIColor.red.setFill()
-        context.fill(CGRect(origin: .zero, size: tightBounds.size))
+        context.fill(CGRect(origin: .zero, size: imageSize))
 
-        // Translate to draw the glyph properly
-        context.translateBy(x: -tightBounds.origin.x, y: -tightBounds.origin.y)
-        CTLineDraw(line, context)
+        // Draw the symbol glyph on top
+        attrString.draw(at: .zero)
 
+        // Capture image
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
 
         guard let cgImage = image?.cgImage else { return nil }
-        self.init(cgImage: cgImage, scale: scale, orientation: .up)
+        self.init(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
     }
 
 
