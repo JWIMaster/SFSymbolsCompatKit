da8b74046ff8ad91b08dce7513054f48a0635cc9
JWI <jwi@j-w-i.org>
2025-10-12 22:25:23 +1100

    a

--- Package.swift
+++ Package.swift
@@ -22,7 +22,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SFSymbolsCompatKit",
-            dependencies: []
+            dependencies: [],
+            resources: [
+                .copy("Assets") // <-- include all your SVGs here
+            ]
         ),
         
             .testTarget(
