11c03d3f011eccfacdfdbd981b68e8f0b5bd8457
JWI <jwi@j-w-i.org>
2025-10-12 22:31:05 +1100

    a

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,27 +7,33 @@ let package = Package(
         .iOS("7.0")
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SFSymbolsCompatKit",
-            targets: ["SFSymbolsCompatKit"]),
+            targets: ["SFSymbolsCompatKit"]
+        ),
     ],
-    dependencies: [
-
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SFSymbolsCompatKit",
             dependencies: [],
             resources: [
-                .copy("Assets") // <-- include all your SVGs here
+                // Manually include every resource file
+                .copy("Assets/lookup.dat"),
+                .copy("Assets/SFSymbols-ultralight.ttf"),
+                .copy("Assets/SFSymbols-thin.ttf"),
+                .copy("Assets/SFSymbols-light.ttf"),
+                .copy("Assets/SFSymbols-regular.ttf"),
+                .copy("Assets/SFSymbols-medium.ttf"),
+                .copy("Assets/SFSymbols-semibold.ttf"),
+                .copy("Assets/SFSymbols-bold.ttf"),
+                .copy("Assets/SFSymbols-heavy.ttf"),
+                .copy("Assets/SFSymbols-black.ttf")
             ]
         ),
-        
-            .testTarget(
-                name: "SFSymbolsCompatKitTests",
-                dependencies: ["SFSymbolsCompatKit"]),
+        .testTarget(
+            name: "SFSymbolsCompatKitTests",
+            dependencies: ["SFSymbolsCompatKit"]
+        ),
     ]
 )
